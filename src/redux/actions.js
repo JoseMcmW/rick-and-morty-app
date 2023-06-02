1407098c073f4fb8d44760acd7fb0ad1019bcd2f
Jose Centeno <josemanuelcm25@gmail.com>
2023-06-02 14:26:24 -0300

    Conexcvion a DB

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,22 +4,34 @@ import axios from "axios";
 export const addFavorite = (character) => {
   const endpoint = "http://localhost:3001/rickandmorty/fav";
   return async (dispatch) => {
-    const { data } = await axios.post(endpoint, character)
-    return dispatch({
-      type: ADD_FAV,
-      payload: data
-    })
+    try {
+      const { data } = await axios.post(endpoint, character);
+
+      if (!data.length) throw Error("No hay favoritos");
+
+      return dispatch({
+        type: ADD_FAV,
+        payload: data,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 };
 
 export const deleteFavorite = (id) => {
   const endpoint = `http://localhost:3001/rickandmorty/fav/${id}`;
   return async (dispatch) => {
-    const { data } = await axios.delete(endpoint)
+    try {
+      const { data } = await axios.delete(endpoint);
+
       return dispatch({
         type: REMOVE_FAV,
         payload: data,
       });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 };
 
